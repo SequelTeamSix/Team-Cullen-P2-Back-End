d3fe47560a93e7341b85b3e3493629c1d211b554
DylanWilson <djmcfarlane7@gmail.com>
2022-03-15 20:44:55 -0400

    filled out player class

--- src/main/java/com/teamcullen/models/Player.java
+++ src/main/java/com/teamcullen/models/Player.java
@@ -1,4 +1,43 @@
 package com.teamcullen.models;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@Table(name = "players")
 public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "player_id")
+    private int player_id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "points")
+    private int points;
+
+    @Column(name = "wins")
+    private int wins;
+
+    @Column(name = "loses")
+    private int loses;
+
+
+
+
+
+
 }
