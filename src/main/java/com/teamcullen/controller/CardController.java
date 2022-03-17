2d40d499673f89a1dc12753295955ae0606d5abe
Cullen <cullenkuch@gmail.com>
2022-03-17 19:49:22 -0400

    Added cross origin annotation to controller methods

--- src/main/java/com/teamcullen/controller/CardController.java
+++ src/main/java/com/teamcullen/controller/CardController.java
@@ -15,41 +15,49 @@ public class CardController {
         this.cardService = cardService;
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/card/name/{card_name}")
     public Card getCardByName(@PathVariable String card_name) {
         return cardService.getCardByName(card_name);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/card/id/{card_id}")
     public Card getCardById(@PathVariable int card_id) {
         return cardService.getCardById(card_id);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/card")
     public List<Card> getAllCards() {
         return cardService.getAllCards();
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/card/power/{power}")
     public List<Card> getAllCardsByPower(@PathVariable int power) {
         return cardService.getAllCardsByPower(power);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("card/power/{power_low}-{power_high}")
     public List<Card> getAllCardsBetweenPowerLevels(@PathVariable int power_low, @PathVariable int power_high) {
         return cardService.getAllCardsBetweenPowerLevels(power_low, power_high);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/card/add")
     public Card postCard(@RequestBody Card card) {
         return cardService.saveCard(card);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @DeleteMapping("/card/delete/{card_id}")
     public int deleteCardById(@PathVariable int card_id) {
         return cardService.deleteCardById(card_id);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @PutMapping("/card/update/{card_id}")
     public Card updateModifierById(@PathVariable int card_id, @RequestBody Card card) {
         return cardService.updateCard(card_id, card);
--- src/main/java/com/teamcullen/controller/PlayerController.java
+++ src/main/java/com/teamcullen/controller/PlayerController.java
@@ -17,34 +17,40 @@ public class PlayerController {
         this.playerService = playerService;
     }
 
-
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/player")
     public List<Player> getAllPlayers(){return playerService.getAllPlayers();}
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping ("/player/id/{player_id}")
     public Player getPlayerById(@PathVariable int player_id){return playerService.getPlayerById(player_id);}
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/player/name/{username}")
     public Player getPlayerByName(@PathVariable String username){
 
         return playerService.getPlayerByName(username);  }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/player/leaderboard")
     public List<Player> getPlayersByWins(){
         return playerService.getPlayersByWins();
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/player/add")
     public Player createPlayer(@RequestBody Player player){
         return playerService.createPlayer(player);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @PutMapping("/player/update/{player_id}")
     public Player updatePlayer(@PathVariable int player_id, @RequestBody Player player){
 
         return playerService.updatePlayer(player_id, player);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @DeleteMapping("/player/delete/{player_id}")
     public void deletePlayerById(@PathVariable int player_id){
 
